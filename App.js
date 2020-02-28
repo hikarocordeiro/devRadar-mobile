a582068f8a28b0ab9092bc3bf895b015c0e70513
Hikaro Cordeiro <hikaro.cordeiro@gmail.com>
2020-02-28 15:30:53 -0300

    adding react navigation

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+
 import {StyleSheet, View, Text} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.sectionTitle}>Step One</Text>
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text style={styles.sectionTitle}>Step One</Text>
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#7159c1',
     alignItems: 'center',
     justifyContent: 'center',
   },
